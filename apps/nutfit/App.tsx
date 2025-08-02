fa8a99e8e86e2c045feab7c64fff12cb98f044b0
QZ15 <47049335+QZ15@users.noreply.github.com>
2025-08-02 15:19:00 -0400

    fix: import gesture handler root

--- apps/nutfit/App.tsx
+++ apps/nutfit/App.tsx
@@ -10,29 +10,32 @@ import { WorkoutFavoritesProvider } from "./src/context/WorkoutFavoritesContext"
 import { WeekWorkoutsProvider } from "./src/context/WeekWorkoutsContext";
 import { RecentWorkoutsProvider } from "./src/context/RecentWorkoutsContext";
 import { CompletedWorkoutsProvider } from "./src/context/CompletedWorkoutsContext";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <MealFilterProvider>
-      <WorkoutFilterProvider>
-        <FavoritesProvider>
-          <WorkoutFavoritesProvider>
-            <TodayMealsProvider>
-              <WeekWorkoutsProvider>
-                <CompletedWorkoutsProvider>
-                  <MealOfTheDayProvider>
-                    <RecentMealsProvider>
-                      <RecentWorkoutsProvider>
-                        <RootNavigator />
-                      </RecentWorkoutsProvider>
-                    </RecentMealsProvider>
-                  </MealOfTheDayProvider>
-                </CompletedWorkoutsProvider>
-              </WeekWorkoutsProvider>
-            </TodayMealsProvider>
-          </WorkoutFavoritesProvider>
-        </FavoritesProvider>
-      </WorkoutFilterProvider>
-    </MealFilterProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <MealFilterProvider>
+        <WorkoutFilterProvider>
+          <FavoritesProvider>
+            <WorkoutFavoritesProvider>
+              <TodayMealsProvider>
+                <WeekWorkoutsProvider>
+                  <CompletedWorkoutsProvider>
+                    <MealOfTheDayProvider>
+                      <RecentMealsProvider>
+                        <RecentWorkoutsProvider>
+                          <RootNavigator />
+                        </RecentWorkoutsProvider>
+                      </RecentMealsProvider>
+                    </MealOfTheDayProvider>
+                  </CompletedWorkoutsProvider>
+                </WeekWorkoutsProvider>
+              </TodayMealsProvider>
+            </WorkoutFavoritesProvider>
+          </FavoritesProvider>
+        </WorkoutFilterProvider>
+      </MealFilterProvider>
+    </GestureHandlerRootView>
   );
 }
