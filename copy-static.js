c64a5d8cc732cad1c64c7430891f6cdfc6bccb1b
QZ15 <47049335+QZ15@users.noreply.github.com>
2025-08-15 03:49:19 -0400

    feat: add landing site hosting

--- copy-static.js
+++ copy-static.js
@@ -3,3 +3,4 @@ import fse from "fs-extra";
 
 fse.copySync("apps/portfolio/frontend/dist", "public/portfolio");
 fse.copySync("apps/glaze/frontend/dist", "public/glaze");
+fse.copySync("apps/landing/dist", "public/landing");
